8f1d3da726ad86f61eba15dc8daf0d303b4fb1db
JRichardsz <jrichardsz.java@gmail.com>
2017-02-11 20:16:05 -0500

    add simple cube

---
+++ 001-TestOpenGL/app/src/main/java/com/example/stark/testopengl/OpenGLDemoActivity.java
@@ -0,0 +1,28 @@
+package com.example.stark.testopengl;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by stark on 09/01/2017.
+ */
+
+public class OpenGLDemoActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Go fullscreen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        GLSurfaceView view = new GLSurfaceView(this);
+        view.setRenderer(new OpenGLRenderer());
+        setContentView(view);
+    }
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# android-3d-templates
+Android 3D Templates
+=================
+
+Usefull Templates of 3D Android Apps
+
+
+# [001 Test OpenGL](/001 Test OpenGL)
+
+> Simple cube
+
+<img src="001-TestOpenGL/home.gif?raw=true" width="150">
